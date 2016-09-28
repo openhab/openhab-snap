245b2784df4400bb4dfdc25ddc15380c1e1c3f99
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-09-28 14:30:36 +0000

    Switching to inline editing
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- parts/plugins/x-openhab.py
+++ parts/plugins/x-openhab.py
@@ -1,14 +1,9 @@
 # -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
 import os
-import stat
 import fileinput
 import sys
 import snapcraft
 import logging
-
-from tempfile import mkstemp
-from shutil import move
-from os import remove, close
 
 from snapcraft.plugins import dump
 
@@ -32,18 +27,7 @@ class OpenHabPlugin(snapcraft.plugins.dump.DumpPlugin):
         self._replaceAll(self.installdir+"/runtime/karaf/bin/karaf", "${KARAF_HOME}/instances", "${SNAP_USER_COMMON}/karaf/instances")
 
     def _replaceAll(self,filePath,searchExp,replaceExp):
-        fh, abs_path = mkstemp()
-        with open(abs_path,'w') as new_file:
-            with open(filePath) as old_file:
-#            for line in fileinput.input(f, inplace=1):
-#                if searchExp in line:
-#                    line = line.replace(searchExp,replaceExp)
-                for line in old_file:
-                    new_file.write(line.replace(searchExp, replaceExp))
-        close(fh)
-        #Remove original file
-        remove(filePath)
-        #Move new file
-        move(abs_path, filePath)
-        st = os.stat(filePath)
-        os.chmod(filePath, st.st_mode | stat.S_IEXEC | stat.S_IXGRP | stat.S_IRGRP | stat.S_IROTH | stat.S_IXOTH)
+        for line in fileinput.input(filePath, inplace=1):
+             if searchExp in line:
+                 line = line.replace(searchExp,replaceExp)
+             sys.stdout.write(line)
