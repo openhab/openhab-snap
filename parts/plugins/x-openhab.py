b7ac61adade4477e8b295976aff997dd774c8e29
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-12-01 15:51:18 +0000

    Fix instance location also for client,shell,instance scripts
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- parts/plugins/x-openhab.py
+++ parts/plugins/x-openhab.py
@@ -14,8 +14,8 @@ class OpenHabPlugin(snapcraft.plugins.dump.DumpPlugin):
     def build(self):
         super().build()
         self._modify_oh2_dir()
-        self._modify_karaf()
         self._modify_setenv()
+        self._fix_instance_path()
 
     def _modify_oh2_dir(self):
         logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/oh2_dir_layout')
@@ -27,9 +27,15 @@ class OpenHabPlugin(snapcraft.plugins.dump.DumpPlugin):
         logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/setenv')
         self._replaceAll(self.installdir+"/runtime/karaf/bin/setenv","-Dopenhab.logdir=${OPENHAB_LOGDIR}","-Dopenhab.logdir=${OPENHAB_LOGDIR}\n  -Duser.home=${SNAP_COMMON}")
 
-    def _modify_karaf(self):
+    def _fix_instance_path(self):
+        logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/client')
+        self._replaceAll(self.installdir+"/runtime/karaf/bin/client", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+        logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/instance')
+        self._replaceAll(self.installdir+"/runtime/karaf/bin/instance", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
         logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/karaf')
         self._replaceAll(self.installdir+"/runtime/karaf/bin/karaf", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+        logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/shell')
+        self._replaceAll(self.installdir+"/runtime/karaf/bin/shell", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
 
     def _replaceAll(self,filePath,searchExp,replaceExp):
         for line in fileinput.input(filePath, inplace=1):
