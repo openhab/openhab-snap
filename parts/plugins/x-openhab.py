56709059fbb4d9e1121d2309bf904266a5bce01e
Ondrej <ondra@ondra.org>
2016-12-04 12:22:32 +0000

    fixing bugs, support for new folder layout (#3)
    * Adding serial port interface
    
    enabling devices like zwave
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Fixing connect-interfaces to work with core as well as ubuntu-core os snap
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * silencing default key value log
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * fixes which command should be run for various apps
    
    - run main service as service, so we don't get console for systemd
    - fix console application
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Redirecting home user to SNAP_COMMON location
    
    this will resolve problems with accessing ~/.karaf
    it will also resolve inconsistency when running apps as different users
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Updating zulu versions for armhf and amd64
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Bumping up version
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Fix instance location also for client,shell,instance scripts
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Adding migration to configure hook
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Bumping up version
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * adjusting snap config to new Restructured folder layout #318
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * more adjustments to new Restructured folder layout #318
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * Improving configure hook
    
    - now handling also install and update
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>
    
    * More improvements to configure
    
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- parts/plugins/x-openhab.py
+++ parts/plugins/x-openhab.py
@@ -14,17 +14,27 @@ class OpenHabPlugin(snapcraft.plugins.dump.DumpPlugin):
     def build(self):
         super().build()
         self._modify_oh2_dir()
-        self._modify_karaf()
+        self._modify_setenv()
+        self._fix_instance_path()
 
     def _modify_oh2_dir(self):
-        logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/oh2_dir_layout')
-        self._replaceAll(self.installdir+"/runtime/karaf/bin/oh2_dir_layout", "${OPENHAB_HOME}/conf", "${SNAP_COMMON}/conf")
-        self._replaceAll(self.installdir+"/runtime/karaf/bin/oh2_dir_layout", "${OPENHAB_HOME}/userdata", "${SNAP_COMMON}/userdata")
-        self._replaceAll(self.installdir+"/runtime/karaf/bin/oh2_dir_layout", "${OPENHAB_RUNTIME}/karaf/etc", "${SNAP_COMMON}/karaf/etc")
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/oh2_dir_layout')
+        self._replaceAll(self.installdir+"/runtime/bin/oh2_dir_layout", "${OPENHAB_HOME}/conf", "${SNAP_COMMON}/conf")
+        self._replaceAll(self.installdir+"/runtime/bin/oh2_dir_layout", "${OPENHAB_HOME}/userdata", "${SNAP_COMMON}/userdata")
 
-    def _modify_karaf(self):
-        logger.warning('Patching ' + self.installdir + '/runtime/karaf/bin/karaf')
-        self._replaceAll(self.installdir+"/runtime/karaf/bin/karaf", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+    def _modify_setenv(self):
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/setenv')
+        self._replaceAll(self.installdir+"/runtime/bin/setenv","-Dopenhab.logdir=${OPENHAB_LOGDIR}","-Dopenhab.logdir=${OPENHAB_LOGDIR}\n  -Duser.home=${SNAP_COMMON}")
+
+    def _fix_instance_path(self):
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/client')
+        self._replaceAll(self.installdir+"/runtime/bin/client", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/instance')
+        self._replaceAll(self.installdir+"/runtime/bin/instance", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/karaf')
+        self._replaceAll(self.installdir+"/runtime/bin/karaf", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
+        logger.warning('Patching ' + self.installdir + '/runtime/bin/shell')
+        self._replaceAll(self.installdir+"/runtime/bin/shell", "${KARAF_HOME}/instances", "${SNAP_COMMON}/karaf/instances")
 
     def _replaceAll(self,filePath,searchExp,replaceExp):
         for line in fileinput.input(filePath, inplace=1):
