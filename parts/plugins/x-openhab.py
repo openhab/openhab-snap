f58952b6cd6614ccae1551cdfd9a287a74c6baf3
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-12-08 12:32:02 +0000

    Switching to openhab-distro compilation
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- parts/plugins/x-openhab.py
+++ parts/plugins/x-openhab.py
@@ -4,18 +4,69 @@ import fileinput
 import sys
 import snapcraft
 import logging
+import shutil
 
-from snapcraft.plugins import dump
+from snapcraft.plugins import jdk, maven, dump
+from snapcraft.internal import sources
+from xml.etree import ElementTree
 
 logger = logging.getLogger(__name__)
 
-class OpenHabPlugin(snapcraft.plugins.dump.DumpPlugin):
+class OpenHabPlugin(snapcraft.BasePlugin):
+
+    @classmethod
+    def schema(cls):
+        schema = super().schema()
+        schema['properties']['distribution'] = {
+            'type': 'string'
+        }
+
+        # Inform Snapcraft of the properties associated with building. If these
+        # change in the YAML Snapcraft will consider the build step dirty.
+        schema['build-properties'].append('distribution')
+
+        return schema
+
+    def __init__(self, name, options, project):
+        super().__init__(name, options, project)
+        self.build_packages.append('maven')
+
+    def _use_proxy(self):
+        return any(k in os.environ for k in ('http_proxy', 'https_proxy'))
+
+    def enable_cross_compilation(self):
+        pass
 
     def build(self):
-        super().build()
+        snapcraft.BasePlugin.build(self)
+
+        tree = ElementTree.parse(os.path.join(self.sourcedir, 'distributions/openhab-' + self.options.distribution, 'pom.xml' ))
+        root = tree.getroot()
+        parent = root.find('{http://maven.apache.org/POM/4.0.0}parent')
+        version = parent.find('{http://maven.apache.org/POM/4.0.0}version').text
+
+        dist_package = os.path.join(self.sourcedir, 'distributions/openhab-online/target/openhab-' + self.options.distribution + '-' + version + '.tar.gz')
+
+        sources.Tar(dist_package, self.builddir).pull()
+        snapcraft.file_utils.link_or_copy_tree(
+            self.builddir, self.installdir,
+            copy_function=lambda src, dst: dump._link_or_copy(src, dst,
+                                                         self.installdir))
         self._modify_oh2_dir()
         self._modify_setenv()
         self._fix_instance_path()
+
+    # to enable launchpad build, we need to call mvn build in pull stage as it
+    # require internet access
+    def pull(self):
+        super().pull()
+        mvn_cmd = ['mvn', 'package']
+        if self._use_proxy():
+            settings_path = os.path.join(self.partdir, 'm2', 'settings.xml')
+            maven._create_settings(settings_path)
+            mvn_cmd += ['-s', settings_path]
+
+        self.run(mvn_cmd, self.sourcedir)
 
     def _modify_oh2_dir(self):
         logger.warning('Patching ' + self.installdir + '/runtime/bin/oh2_dir_layout')
