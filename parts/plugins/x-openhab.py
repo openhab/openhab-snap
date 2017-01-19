77e52fa6a3646a95ad0b55ac5daa8046bfe6e295
Ondrej Kubik <ondrej.kubik@canonical.com>
2017-01-19 20:45:02 +0000

    Fixing maven build on armhf host
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- parts/plugins/x-openhab.py
+++ parts/plugins/x-openhab.py
@@ -5,6 +5,7 @@ import sys
 import snapcraft
 import logging
 import shutil
+import platform
 
 from snapcraft.plugins import jdk, maven, dump
 from snapcraft.internal import sources
@@ -17,6 +18,7 @@ class OpenHabPlugin(snapcraft.BasePlugin):
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
         self.build_packages.append('maven')
+        self.jredir = os.path.join(self.partdir, 'jre')
 
     def _use_proxy(self):
         return any(k in os.environ for k in ('http_proxy', 'https_proxy'))
@@ -33,7 +35,11 @@ class OpenHabPlugin(snapcraft.BasePlugin):
             maven._create_settings(settings_path)
             mvn_cmd += ['-s', settings_path]
 
-        # self.run(mvn_cmd, self.sourcedir + '/distributions/openhab')
+        if platform.machine() == 'armv7l':
+            logger.warning('Setting up zulu jre for maven build')
+            os.environ['JAVA_HOME'] = os.path.join(self.jredir, 'jre')
+            os.environ['PATH'] = os.path.join(self.jredir, 'bin') + os.environ.get('PATH', 'not-set')
+
         self.run(mvn_cmd, self.sourcedir)
 
         tree = ElementTree.parse(os.path.join(self.sourcedir, 'distributions/openhab/pom.xml' ))
@@ -56,6 +62,13 @@ class OpenHabPlugin(snapcraft.BasePlugin):
     # require internet access
     def pull(self):
         super().pull()
+        if platform.machine() == 'armv7l':
+            logger.warning('Running on armhf host, use zulu jre for maven')
+            os.makedirs(self.jredir, exist_ok=True)
+            class Options:
+                source='https://www.azul.com/downloads/zulu/zdk-8-ga-linux_aarch32hf.tar.gz'
+
+            snapcraft.sources.get(self.jredir, None, Options())
 
     def _modify_oh2_dir(self):
         logger.warning('Patching ' + self.installdir + '/runtime/bin/oh2_dir_layout')
